2bf0c87e1058fa2b63d1e144e4e8d2a87b76ee7b
Frederic GINIOUX <frederic.ginioux@nurun.com>
2015-06-25 09:34:25 -0400

    Debug

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("Yeppy !!!!!");
+try {
+  var staticApp     = require('node-static')
+    , grunt         = require('grunt')
+    , colors        = require('colors')
+    , staticDir     = '.'
+    , port          = 5001
+    , traceur       = process.env.TRACEUR || 'none'
+    , gruntTask     = process.env.TRACEUR || 'default';
+
+  if(traceur){
+    staticDir = staticDir + '/' + traceur.replace('-', '/');
+  }
+
+  var file = new staticApp.Server(staticDir);
+
+  console.log(gruntTask, staticDir);
+
+  // --> Run Grunt task
+  grunt.tasks(gruntTask, {verbose: true});
+
+  // --> Create HTTP local static server
+  require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+          file.serve(request, response);
+      }).resume();
+  }).listen(port);
+
+  // --> Log msg server OK 
+  console.log(("Server running on " + (new String(port)).underline + " port... with traceur " + traceur.toUpperCase().underline).green);
+} catch(error) {
+  console.log(error.message);
+}
